ee4e353c0c418ad35bde5cb97d0fe395bdc4b09b
joshuakwan <garfilone@gmail.com>
2016-02-26 14:44:27 +0800

    update compose

--- townhall/townhall.sh
+++ townhall/townhall.sh
@@ -4,8 +4,6 @@
 echo "PYTHONPATH=$PYTHONPATH:/townhall/ansible-townhall" >> ~/.bashrc
 
 # change db host
-sed -i "s|127.0.0.1|mongodb|" /townhall/ansible-townhall/job_persistence/db_connection.py
-sed -i "s|localhost|mongodb|" /townhall/ansible-townhall/code_repo/ansible_repo_mgr.py
 
 # ansible.cfg
 mkdir -p /usr/share/ansible/plugins/callback
