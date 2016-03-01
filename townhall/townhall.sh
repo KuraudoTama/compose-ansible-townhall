5eccc7bf5bde6b2ebd57413ea97b0df8274935ab
joshuakwan <garfilone@gmail.com>
2016-03-01 15:09:29 +0800

    update the build files to import PYTHONPATH

--- townhall/townhall.sh
+++ townhall/townhall.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-# PYTHONPATH
-echo "PYTHONPATH=$PYTHONPATH:/townhall/ansible-townhall" >> ~/.bashrc
-
 # ansible.cfg
 mkdir -p /usr/share/ansible/plugins/callback
 cp -f /opt/ansible/ansible/examples/ansible.cfg /etc/ansible/ansible.cfg
 sed -i "s|#callback_plugins|callback_plugins|" /etc/ansible/ansible.cfg
-cp -f /townhall/ansible-townhall/job_logging/ansible_job_logger.py /usr/share/ansible/plugins/callback/
+cp -f /townhall/ansible-townhall/job_service/ansible_logger_plugin.py /usr/share/ansible/plugins/callback/
 
 # ansible-playbook
 ln -s `which ansible-playbook` -t /usr/bin
